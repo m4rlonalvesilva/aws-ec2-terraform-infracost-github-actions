8d76244345be66517f2371e2953e5553bfede9b4
m4rlonalvesilva <marlonesx@gmail.com>
2022-07-11 02:03:53 -0300

    github-action

---
+++ oic-provider.tf
@@ -0,0 +1,35 @@
+# Passo 1: Adicione o provedor de identidade à AWS
+
+resource "aws_iam_openid_connect_provider" "githubOidc" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = ["a031c46782e6e6c662c2c87c76da9aa62ccabd8e"]
+}
+
+# Passo 2: Configure uma política de assumir-função
+
+data "aws_iam_policy_document" "github_allow" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.githubOidc.arn]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:${GitHubOrg}/${GitHubRepo}:*"]
+
+    }
+  }
+}
+
+resource "aws_iam_role" "github_role" {
+  name               = "GithubActionsRole"
+  assume_role_policy = data.aws_iam_policy_document.github_allow.json
+}
